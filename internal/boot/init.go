2d6a42466be8b5f286f2fda9aee357192dd845cc
it00021hot <liu15077731547@gmail.com>
2025-03-25 18:59:36 +0800

    修复重启文件不存在问题

--- internal/boot/init.go
+++ internal/boot/init.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"github.com/gogf/gf/v2/frame/g"
 	"github.com/gogf/gf/v2/os/gcron"
+	"github.com/gogf/gf/v2/os/gfile"
 	"heygem-api/internal/service"
 )
 
@@ -17,7 +18,20 @@ func Init(ctx context.Context) {
 	VideoPath = g.Config().MustGet(ctx, "heygem.path.video").String()
 	TTSApi = g.Config().MustGet(ctx, "heygem.tts").String()
 	F2FApi = g.Config().MustGet(ctx, "heygem.f2f").String()
+	InitWeb(ctx)
 	_, _ = gcron.AddSingleton(ctx, "*/5 * * * * *", func(ctx context.Context) {
 		service.Video().LoopPending(ctx)
 	})
+}
+
+func InitWeb(ctx context.Context) {
+	if gfile.Exists("resource/assets") {
+		if gfile.Exists("resource/public/assets") {
+			_ = gfile.RemoveAll("resource/public/assets")
+		}
+		_ = gfile.Move("resource/assets", "resource/public/assets")
+		_ = gfile.Move("resource/favicon.ico", "resource/public/")
+		_ = gfile.Move("resource/index.html", "resource/public/")
+	}
+
 }
